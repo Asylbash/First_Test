93700f066117b3d6c5eafb079b290c7b0f98c6d9
ZHYLDYZ ADYLCHAEVA <165602940+Asylbash@users.noreply.github.com>
2026-03-15 19:55:46 +0100

    Update TextBoxPage.java
    Add some methods to test checkbox

--- src/test/java/Pages/TextBoxPage.java
+++ src/test/java/Pages/TextBoxPage.java
@@ -1,4 +1,14 @@
 package Pages;
 
 public class TextBoxPage {
+   @Test
+    void successfulSearchTest() {
+
+        open("https://www.demoqa.com/text-box");
+        $("[id=userName]").setValue("Vanessa");
+        $("[id=userEmail]").setValue("Test@test.com");
+        $("[id=currentAddress]").setValue("via Roma 1, Rome , Italy");
+      $("[id=permanentAddress]").setValue("via Roma 1, Rome , Italy");
+      $("[id=submit]").click();
+    }
 }
