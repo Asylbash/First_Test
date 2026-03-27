7681c13e5924db764263e04ad495e267790b804d
ZHYLDYZ ADYLCHAEVA <165602940+Asylbash@users.noreply.github.com>
2026-03-27 19:17:28 +0100

    Update TextBoxTestClassWork.java
    Change for create some conflicts

--- src/test/java/tests/TextBoxTestClassWork.java
+++ src/test/java/tests/TextBoxTestClassWork.java
@@ -21,13 +21,13 @@ public class TextBoxTestClassWork {
     @Test
     void fillFormTest() {
         open("/text-box");
-        $("#userName").setValue("Alex");
+        $("#userName").setValue("Velria");
         $("#userEmail").setValue("alex@egorov.com");
         $("#currentAddress").setValue("Some street 1");
         $("#permanentAddress").setValue("Another street 1");
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Alex"));
+        $("#output #name").shouldHave(text("Valeria"));
         $("#output #email").shouldHave(text("alex@egorov.com"));
         $("#output #currentAddress").shouldHave(text("Some street 1"));
         $("#output #permanentAddress").shouldHave(text("Another street 1"));
