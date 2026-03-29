de51ffc3ac33e9903334a98a4003ea48502dae1d
ZHYLDYZ ADYLCHAEVA <asylbash@gmail.com>
2026-03-30 01:10:05 +0200

    PracticeFormNegativeCases

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PracticeFormTest extends TestBase {
 
@@ -52,5 +53,107 @@ public class PracticeFormTest extends TestBase {
         $(".table-responsive").shouldHave(text("Mario Rossi"));
         $(".table-responsive").shouldHave(text("test@test.com"));
     }
+    @Test
+    void emptyFormTest() {
 
+        open("/automation-practice-form");
+        executeJavaScript("""
+                document.getElementById('fixedban')?.remove();
+                document.querySelector('footer')?.remove();
+                """);
+
+        $("#submit").click();
+
+        $("#firstName").shouldBe(empty);
+        $("#lastName").shouldBe(empty);
+        $("#userNumber").shouldBe(empty);
+    }
+    @Test
+    void emptyGenderFormTest() {
+
+        open("/automation-practice-form");
+
+        executeJavaScript("""
+                document.getElementById('fixedban')?.remove();
+                document.querySelector('footer')?.remove();
+                """);
+
+        $("#firstName").setValue("Mario");
+        $("#lastName").setValue("Rossi");
+
+        $("#userEmail").setValue("test@test.com");
+        $("#userNumber").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__day--015:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").setValue("Eng").pressEnter();
+
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+
+        $("#uploadPicture").uploadFromClasspath("pictures/foto (1).png");
+
+        $("#currentAddress").setValue("Test address");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#submit").click();
+        $("#userForm").shouldHave(cssClass("was-validated"));
+        $(".modal-content").shouldNot(appear);
+
+    }
+    @Test
+    void invalidEmailTest() {
+
+        open("/automation-practice-form");
+
+        executeJavaScript("""
+                document.getElementById('fixedban')?.remove();
+                document.querySelector('footer')?.remove();
+                """);
+
+        $("#firstName").setValue("Mario");
+        $("#lastName").setValue("Rossi");
+        $("#userEmail").setValue("invalidEmail");
+        $("#genterWrapper").$(byText("Male")).click();
+
+        $("#userNumber").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__day--015:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").setValue("Eng").pressEnter();
+
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+
+        $("#uploadPicture").uploadFromClasspath("pictures/foto (1).png");
+
+        $("#currentAddress").setValue("Test address");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#submit").click();
+
+        $("#userForm").shouldHave(cssClass("was-validated"));
+
+        String isInvalid = executeJavaScript(
+                "return document.getElementById('userEmail').matches(':invalid')"
+        ).toString();
+
+        assertEquals("true", isInvalid);
+
+        $("#userEmail").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+    }
 }
